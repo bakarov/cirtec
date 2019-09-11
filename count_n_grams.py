9e78179a2046a8d3c10bc617de2c7ba82d990782
bakarov <amirbakarov@gmail.com>
2019-09-11 18:18:52 +0300

    refactor n grams counter; replace gensim.phrases with nltk.ngrams

--- count_n_grams.py
+++ count_n_grams.py
@@ -2,52 +2,15 @@ import re
 import json
 import optparse
 from collections import Counter, defaultdict
-from operator import itemgetter
-from gensim.models.phrases import Phrases, Phraser
 from ufal.udpipe import Model, Pipeline
-from os import path, makedirs
+from os import path, makedirs, listdir
 from langdetect import detect_langs
 from typing import Pattern, Tuple, List, Dict
-
-nltk_stopwords_en = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll",
-                     "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's",
-                     'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs',
-                     'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am',
-                     'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does',
-                     'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while',
-                     'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during',
-                     'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over',
-                     'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all',
-                     'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only',
-                     'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't",
-                     'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't",
-                     'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't",
-                     'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn',
-                     "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won',
-                     "won't", 'wouldn', "wouldn't"]
-
-nltk_stopwords_ru = ['и', 'в', 'во', 'не', 'что', 'он', 'на', 'я', 'с', 'со', 'как', 'а', 'то', 'все', 'она', 'так',
-                     'его', 'но', 'да',
-                     'ты', 'к', 'у', 'же', 'вы', 'за', 'бы', 'по', 'только', 'ее', 'мне', 'было', 'вот', 'от', 'меня',
-                     'еще', 'нет', 'о',
-                     'из', 'ему', 'теперь', 'когда', 'даже', 'ну', 'вдруг', 'ли', 'если', 'уже', 'или', 'ни', 'быть',
-                     'был', 'него', 'до',
-                     'вас', 'нибудь', 'опять', 'уж', 'вам', 'ведь', 'там', 'потом', 'себя', 'ничего', 'ей', 'может',
-                     'они', 'тут', 'где',
-                     'есть', 'надо', 'ней', 'для', 'мы', 'тебя', 'их', 'чем', 'была', 'сам', 'чтоб', 'без', 'будто',
-                     'чего', 'раз', 'тоже',
-                     'себе', 'под', 'будет', 'ж', 'тогда', 'кто', 'этот', 'того', 'потому', 'этого', 'какой', 'совсем',
-                     'ним', 'здесь',
-                     'этом', 'один', 'почти', 'мой', 'тем', 'чтобы', 'нее', 'сейчас', 'были', 'куда', 'зачем', 'всех',
-                     'никогда', 'можно',
-                     'при', 'наконец', 'два', 'об', 'другой', 'хоть', 'после', 'над', 'больше', 'тот', 'через', 'эти',
-                     'нас', 'про',
-                     'всего', 'них', 'какая', 'много', 'разве', 'три', 'эту', 'моя', 'впрочем', 'хорошо', 'свою',
-                     'этой', 'перед', 'иногда',
-                     'лучше', 'чуть', 'том', 'нельзя', 'такой', 'им', 'более', 'всегда', 'конечно', 'всю', 'между']
+from nltk import ngrams
 
 
-def preprocess(pipeline, sentence, lemmatize=True, add_pos=False, punct_tag='PUNCT', text_entry_char=4):
+def preprocess(pipeline, sentence: str, stopwords: List, lemmatize: bool = True, punct_tag: str = 'PUNCT',
+               text_entry_char: int = 4):
     indent = 4
     word_id = 1
     lemma_id = 2
@@ -58,14 +21,12 @@ def preprocess(pipeline, sentence, lemmatize=True, add_pos=False, punct_tag='PUN
             word = parsed_word.split('\t')[word_id].lower()
             lemma = parsed_word.split('\t')[lemma_id]
             pos = parsed_word.split('\t')[pos_id]
-            if pos == punct_tag:
+            if pos == punct_tag or word in stopwords:
                 continue
             if lemmatize:
-                if lemma not in nltk_stopwords_en and lemma not in nltk_stopwords_ru:
-                    tokenized_par.append(lemma)
+                tokenized_par.append(lemma)
             else:
-                if word not in nltk_stopwords_en and word not in nltk_stopwords_ru:
-                    tokenized_par.append(word)
+                tokenized_par.append(word)
     return tokenized_par[text_entry_char:]
 
 
@@ -89,9 +50,13 @@ def normalize_sentence(sentence: str, re_pattern_1: Pattern, re_pattern_2: Patte
     return re_pattern_1.sub('', re_pattern_2.sub('', sentence.replace('et al', '').replace('\xad ', '').lower()))
 
 
-def load_data(filename: str, manual_language=None) -> Tuple[List, List, List]:
+def load_data(filename: str, lemmatize: bool, stopwords_path: str, manual_language=None) -> Tuple[List, List]:
     with open(filename, 'r') as f:
-        citations = f.read().split('\n')
+        citations = f.read().split('\n')[:100]
+    stopwords = []
+    for stopwords_list in listdir(stopwords_path):
+        with open(path.join(stopwords_path, stopwords_list), 'r') as f:
+            stopwords.extend(f.read().split('\n'))
     # Download models here: https://lindat.mff.cuni.cz/repository/xmlui/handle/11234/1-2998#
     en_model = Model.load(path.join('.', 'udpipe', 'english-ewt-ud-2.4-190531.udpipe'))
     en_pipeline = Pipeline(en_model, 'tokenize', Pipeline.DEFAULT, Pipeline.DEFAULT, 'conllu')
@@ -101,12 +66,10 @@ def load_data(filename: str, manual_language=None) -> Tuple[List, List, List]:
     pattern_brackets = re.compile('[\(\[].*?[\)\]]')
     citations_ids = []
     citations_texts = []
-    citations_texts_lemma = []
     citation_id_position = 4
     for citation in citations:
         try:
             citation_id = '_'.join(citation.split()[:citation_id_position])
-            citation_id = citation.split()[1]
         except IndexError:
             print('Citation ID was not recognized for {}'.format(citation))
             continue
@@ -117,44 +80,44 @@ def load_data(filename: str, manual_language=None) -> Tuple[List, List, List]:
             print('Language has not been detected for {}'.format(citation_id))
             continue
         citations_ids.append(citation_id)
-        citations_texts.append(preprocess(pipeline, normalized_citation, lemmatize=False))
-        citations_texts_lemma.append(preprocess(pipeline, normalized_citation, lemmatize=True))
-    return citations_ids, citations_texts, citations_texts_lemma
+        citations_texts.append(preprocess(pipeline, normalized_citation, stopwords, lemmatize=lemmatize))
+    return citations_ids, citations_texts
 
 
-def get_n_grams(tokens: List, n: int, min_count: int = 1, threshold: int = 1, delimiter=b'_') -> List:
-    tokens_ = tokens
-    n_grams = []
-    for i in range(n):
-        n_gram = Phrases(tokens_, min_count=min_count, delimiter=delimiter, threshold=threshold)
-        n_gram_phraser = Phraser(n_gram)
-        tokens__ = n_gram_phraser[tokens_]
-        n_grams.append(tokens__)
-        tokens_ = tokens__
-    return n_grams
+def get_n_grams_local_counts(citations_texts: List, n: int, delimiter=' ') -> List:
+    n_grams_with_counts = []
+    for citations_text in citations_texts:
+        n_grams = [delimiter.join(n_gram_tuples) for n_gram_tuples in list(ngrams(citations_text, n))]
+        n_grams_counts = dict(Counter(n_grams))
+        n_grams_with_counts.append(n_grams_counts)
+    return n_grams_with_counts
 
 
-def get_result(n_grams: List, citations_ids: List, max_n: int, output_path: str, min_n: int=2):
-    result = defaultdict(lambda: defaultdict(lambda: []))
-    for n in range(min_n, max_n):
-        result_key = '{}-grams'.format(n)
-        n_grams_global_counts = dict(Counter(token for sentence in n_grams[n] for token in set(sentence)))
-        sentences_ids = {}
-        for word in n_grams_global_counts.keys():
-            sentences_ids[word] = [sentence_id_ for sentence_id_, sentence_ in enumerate(n_grams[n]) if
-                                   word in sentence_]
-        for sentence_id, sentence in enumerate(n_grams[n]):
-            n_grams_local_counts = dict(Counter(n_grams[n][sentence_id]))
-            citations_id = citations_ids[sentence_id]
-            for word in sentence:
-                word_result = {}
-                word_result['w'] = word
-                word_result['n'] = n_grams_global_counts[word]
-                word_result['t'] = n_grams_local_counts[word]
-                word_result['c'] = itemgetter(*sentences_ids[word])(citations_ids)
-                result[result_key][citations_id].append(word_result)
-    with open(output_path, 'w', encoding='utf-8') as f:
-        json.dump(dict(result), f, ensure_ascii=False)
+def get_n_grams_global_counts(n_grams: List, threshold: int) -> Dict:
+    all_global_counts = defaultdict(lambda: {'count': 0, 'ids': []})
+    threshold_global_counts = defaultdict(lambda: {})
+    for sentence_id, sentence in enumerate(n_grams):
+        for n_gram, n_gram_count in sentence.items():
+            if n_gram not in threshold_global_counts:
+                current_count = all_global_counts[n_gram]['count'] + n_gram_count
+                all_global_counts[n_gram]['ids'].append({citations_ids[sentence_id]: n_gram_count})
+                if current_count >= threshold:
+                    threshold_global_counts[n_gram]['count'] = current_count
+                    threshold_global_counts[n_gram]['ids'] = all_global_counts[n_gram]['ids']
+                else:
+                    all_global_counts[n_gram]['count'] = current_count
+            else:
+                threshold_global_counts[n_gram]['count'] += n_gram_count
+                threshold_global_counts[n_gram]['ids'].append({citations_ids[sentence_id]: n_gram_count})
+    return dict(threshold_global_counts)
+
+
+def serialize_n_grams_counts(n_grams_counts: Dict, n: int, output_dir):
+    try:
+        with open(path.join(output_dir, '{}-gram-result.json'.format(n)), 'w', encoding='utf-8') as f:
+            json.dump(n_grams_counts, f, ensure_ascii=False)
+    except FileNotFoundError:
+        makedirs(output_dir)
 
 
 if __name__ == '__main__':
@@ -162,18 +125,20 @@ if __name__ == '__main__':
     parser.add_option('-i', '--input',
                       help='Path to the input file', default='./data/citcon4bundles.txt')
     parser.add_option('-o', '--output',
-                      help='Path to the output directory', default='data/')
-    parser.add_option('-c', '--count',
-                      help='Ignore all words and bigrams with total collected count lower than this value', default=1)
+                      help='Path to the output directory', default='result')
     parser.add_option('-t', '--threshold',
-                      help='Represent a score threshold for forming the phrases (higher means fewer phrases).',
-                      default=1)
-    parser.add_option('-n', '--n',
-                      help='Maximum n for formed n-grams', default=6)
+                      help='Minimum global count for n-grams.', default=5)
+    parser.add_option('-n', '--max_n',
+                      help='Maximum n for formed n-grams', default=3)
+    parser.add_option('-m', '--min_n',
+                      help='Minimum n for formed n-grams', default=2)
+    parser.add_option('-l', '--lemmatization',
+                      help='Enable lemmatization for texts', default=True)
+    parser.add_option('-s', '--stopwords_path',
+                      help='Path to stop words lists', default='utils')
     options, args = parser.parse_args()
-    max_n = options.n - 1
-    citations_ids, citations_texts, citations_texts_lemma = load_data(options.input)
-    n_grams = get_n_grams(citations_texts, n=max_n, min_count=options.count, threshold=options.threshold)
-    n_grams_lemma = get_n_grams(citations_texts_lemma, n=max_n, min_count=options.count, threshold=options.threshold)
-    get_result(n_grams, citations_ids, max_n, path.join(options.output, 'result.json'))
-    get_result(n_grams_lemma, citations_ids, max_n, path.join(options.output, 'result_lemma.json'))
+    citations_ids, citations_texts = load_data(options.input, options.lemmatization, options.stopwords_path)
+    for n in range(options.min_n, options.max_n + 1):
+        n_grams_counts = get_n_grams_local_counts(citations_texts, n)
+        global_n_grams_counts = get_n_grams_global_counts(n_grams_counts, options.threshold)
+        serialize_n_grams_counts(global_n_grams_counts, n, options.output)
